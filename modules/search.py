b70d6002ad34e3d0d158b7bdd423e806cf2d577c
Jenniferlore <61295811+Jenniferlore@users.noreply.github.com>
2020-06-12 16:28:58 -0500

    Se creó una parte del diccionario

--- modules/search.py
+++ modules/search.py
@@ -1,1 +1,3 @@
-print('en modules')
+#Creamos el diccionario con las relaciones
+
+ciudades = {'P': ['Tc','Tz','V'], 'Tz': ['P','X'], 'Tc': ['P','V'], 'V': ['P','Tc','X','Z'], 'X': ['Tz','V','Z','B','H'], 'Z': ['V','X','B'], 'H': ['X','F'], 'B': ['Z','X','J','A'], 'F': ['H','Y','HJ'], 'C': ['S','M']}
