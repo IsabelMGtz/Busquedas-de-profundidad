7313d56b723810e7624181b59939161c639d77fc
Jenniferlore <61295811+Jenniferlore@users.noreply.github.com>
2020-06-18 18:35:17 -0500

    Se creó el código de la búsqueda

--- main_program.py
+++ main_program.py
@@ -1,1 +1,24 @@
-print('en main')
+#Importamos el diccionario de los vecinos
+from modules.search import ciudades
+from typing import List
+
+#Definimos nuestra función que realizara la busqueda de profundidad
+
+def profundidad(mapa:str, x:str, y:str) -> List[str]:
+   #Creamos el árbol y la lista que guardara la ruta
+   tree = []
+   route = []
+   tree.append(x)
+   route.append(x)
+   while tree:
+      city = tree.pop()
+      if city == y:
+         return route
+      #Recorremos por cada vecino 
+      for i in mapa[city]:
+         if i not in route:
+            tree.append(i)
+            route.append(i)
+          
+#Probamos a ver que sale :'v           
+profundidad(ciudades, 'X', 'B')
